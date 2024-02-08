d9d34f123e005b89d71368d0843f03db41c47f81
RappyTV <rappytv@rappytv.com>
2024-02-08 12:35:31 +0100

    Send labyconnect messages to the chat, add "buttons" to reply and mark as read

--- core/src/main/java/com/rappytv/labychatutils/listeners/LabyChatListener.java
+++ core/src/main/java/com/rappytv/labychatutils/listeners/LabyChatListener.java
@@ -1,13 +1,16 @@
 package com.rappytv.labychatutils.listeners;
 
 import com.rappytv.labychatutils.LabyChatUtilsAddon;
+import net.labymod.api.Laby;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.component.event.ClickEvent;
 import net.labymod.api.client.component.event.HoverEvent;
 import net.labymod.api.client.component.format.NamedTextColor;
 import net.labymod.api.client.component.format.TextDecoration;
 import net.labymod.api.event.Subscribe;
+import net.labymod.api.event.labymod.labyconnect.session.chat.LabyConnectChatMessageEvent;
 import net.labymod.api.event.labymod.labyconnect.session.request.LabyConnectIncomingFriendRequestAddEvent;
+import net.labymod.api.labyconnect.protocol.model.chat.TextChatMessage;
 
 public class LabyChatListener {
 
@@ -43,6 +46,41 @@ public class LabyChatListener {
                         .clickEvent(ClickEvent.runCommand(
                             "/lcu decline " + event.request().getName()
                         ))
+                )
+        );
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Subscribe
+    public void onChatReceive(LabyConnectChatMessageEvent event) {
+        TextChatMessage message = (TextChatMessage) event.message();
+        if(event.labyConnect().getSession() == null) return;
+        if(message.sender() == event.labyConnect().getSession().self()) return;
+        Laby.references().chatExecutor().displayClientMessage(
+            Component.empty()
+                .append(Component.text("[", NamedTextColor.DARK_GRAY))
+                .append(Component.text("LCU", NamedTextColor.DARK_BLUE)
+                    .decorate(TextDecoration.BOLD)
+                )
+                .append(Component.text("] ", NamedTextColor.DARK_GRAY))
+                .append(Component.text(message.sender().getName(), NamedTextColor.AQUA))
+                .append(Component.text(" » ", NamedTextColor.DARK_GRAY))
+                .append(Component.text(message.getRawMessage(), NamedTextColor.WHITE))
+                .append(Component.text(" ✔", NamedTextColor.GREEN)
+                    .hoverEvent(HoverEvent.showText(
+                        Component.translatable("labychatutils.messages.read")
+                            .color(NamedTextColor.GREEN)
+                    ))
+                    .clickEvent(ClickEvent.runCommand("/lcu read " + "id"))
+                )
+                .append(Component.text(" ➥", NamedTextColor.BLUE)
+                    .hoverEvent(HoverEvent.showText(
+                        Component.translatable("labychatutils.messages.reply")
+                            .color(NamedTextColor.BLUE)
+                    ))
+                    .clickEvent(ClickEvent.suggestCommand(
+                        "/lcu reply " + message.sender().getName() + " "
+                    ))
                 )
         );
     }
