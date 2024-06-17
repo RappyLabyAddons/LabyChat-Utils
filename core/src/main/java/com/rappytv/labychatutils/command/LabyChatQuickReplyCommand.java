c82f9f6029eddee5a0fdf5b73d3e1733a113efea
RappyTV <rappytv@rappytv.com>
2024-06-17 22:04:51 +0200

    Add quick reply command

---
+++ core/src/main/java/com/rappytv/labychatutils/command/LabyChatQuickReplyCommand.java
@@ -0,0 +1,57 @@
+package com.rappytv.labychatutils.command;
+
+import com.rappytv.labychatutils.LabyChatUtilsAddon;
+import net.labymod.api.Laby;
+import net.labymod.api.client.chat.command.Command;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.component.format.NamedTextColor;
+import net.labymod.api.labyconnect.LabyConnectSession;
+import net.labymod.api.labyconnect.protocol.model.chat.Chat;
+import net.labymod.api.labyconnect.protocol.model.chat.ChatMessage;
+
+public class LabyChatQuickReplyCommand extends Command {
+
+    private static Chat recentChat = null;
+
+    public LabyChatQuickReplyCommand() {
+        super("lcr");
+    }
+
+    @Override
+    public boolean execute(String prefix, String[] arguments) {
+        LabyConnectSession session = Laby.references().labyConnect().getSession();
+        if(session == null) {
+            displayMessage(LabyChatUtilsAddon.prefix.copy().append(Component.translatable(
+                "labychatutils.messages.notConnected",
+                NamedTextColor.RED
+            )));
+            return true;
+        }
+
+        if(recentChat == null) {
+            displayMessage(LabyChatUtilsAddon.prefix.copy().append(Component.translatable(
+                "labychatutils.messages.noRecentChat",
+                NamedTextColor.RED
+            )));
+            return true;
+        }
+        if(arguments.length < 1) {
+            displayMessage(LabyChatUtilsAddon.prefix.copy().append(Component.translatable(
+                "labychatutils.messages.enterText",
+                NamedTextColor.RED
+            )));
+            return true;
+        }
+
+        recentChat.sendMessage(String.join(" ", arguments));
+        for(ChatMessage msg : recentChat.getMessages()) {
+            if(!msg.isRead())
+                msg.markAsRead();
+        }
+        return true;
+    }
+
+    public static void setRecentChat(Chat chat) {
+        recentChat = chat;
+    }
+}
--- core/src/main/java/com/rappytv/labychatutils/listeners/LabyChatListener.java
+++ core/src/main/java/com/rappytv/labychatutils/listeners/LabyChatListener.java
@@ -2,6 +2,7 @@ package com.rappytv.labychatutils.listeners;
 
 import com.rappytv.labychatutils.LabyChatUtilsAddon;
 import com.rappytv.labychatutils.LabyChatUtilsConfig;
+import com.rappytv.labychatutils.command.LabyChatQuickReplyCommand;
 import net.labymod.api.Laby;
 import net.labymod.api.client.component.Component;
 import net.labymod.api.client.component.event.ClickEvent;
@@ -116,6 +117,7 @@ public class LabyChatListener {
     @SuppressWarnings("ConstantConditions")
     @Subscribe
     public void onChatReceive(LabyConnectChatMessageEvent event) {
+        LabyChatQuickReplyCommand.setRecentChat(event.chat());
         if(!config.showAnyMessages()) return;
         TextChatMessage message = (TextChatMessage) event.message();
         if(event.labyConnect().getSession() == null) return;
